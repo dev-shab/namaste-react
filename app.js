0a5f1598124dabe083719f80e0289c7fa268c674
dev-shab <shabreeshnair1998@gmail.com>
2024-11-13 20:45:11 +0530

    episode-03|Laying the Foundation

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    className: "heading",
-  },
-  "Hello World from React!!!"
+const Title = () => <h1 id="head">Namaste React using JSX</h1>;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1 id="heading">Namaste React Functional Component</h1>
+  </div>
 );
-const heading2 = React.createElement(
-  "h1",
-  {
-    className: "heading",
-  },
-  "Don't worry, be Happy!"
-);
-const child = React.createElement("div", { id: "child" }, [heading1, heading2]);
-const parent = React.createElement("div", { id: "parent" }, child);
+
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(parent);
+root.render(<HeadingComponent />);
